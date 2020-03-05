4284b04367ed0317d11308ddc36f36b37652dd56
AuGom <augusto.pg@live.com>
2020-03-05 16:14:35 -0300

    App working

--- HeadOrTails/app/src/main/java/com/example/headortails/MainActivity.java
+++ HeadOrTails/app/src/main/java/com/example/headortails/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.headortails;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button play=findViewById(R.id.playButton);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(), ResultActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+
 }
--- HeadOrTails/app/src/main/java/com/example/headortails/ResultActivity.java
+++ HeadOrTails/app/src/main/java/com/example/headortails/ResultActivity.java
@@ -2,13 +2,40 @@ package com.example.headortails;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class ResultActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        ImageView coin=findViewById(R.id.coin);
+        Button back=findViewById(R.id.backButton);
+
+        int rand=new Random().nextInt(2);
+        if(rand==0){
+            coin.setImageResource(R.drawable.moeda_cara);
+        }else{
+            coin.setImageResource(R.drawable.moeda_coroa);
+        }
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
